7301eba597e41020d8802deb88dc8583dc68b089
Amy Y <ctrlalternativedelete@gmail.com>
2021-04-15 01:56:49 +1000

    Create unitylibs.py

---
+++ modules/unitylibs.py
@@ -0,0 +1,13 @@
+from html.parser import HTMLParser
+import urllib.request
+
+class UnityLibsUtils:
+    def __init__(self):
+        self.unityVersion = "2020.1.5"
+        self.githubRawUrl = f"https://github.com/LavaGang/Unity-Runtime-Libraries/raw/master/{self.unityVersion}.zip"
+
+
+
+
+
+
