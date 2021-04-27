35b41815a78cc023c577c5445e782b5fd372d97d
Amy <ctrlalternativedelete@gmail.com>
2021-04-27 15:10:00 +1000

    added theme

--- srxdbepinexinstallerui.py
+++ srxdbepinexinstallerui.py
@@ -1,43 +1,57 @@
 from tkinter import * 
-from tkinter import filedialog
+from tkinter import ttk
 from modules.steamutils import SteamUtils
+from modules.bepinex import BepInExUtils
 
 class GUIWindow:
     def __init__(self, win):
+        # Init Modules
         self.steamutils = SteamUtils()
+        self.bepinutils = BepInExUtils()
+        self.selectedVersion = StringVar()
 
-        n_rows =2
-        n_columns =3
+        # Theme
+        style = ttk.Style(win)
+        style.theme_names()
+        win.tk.call('source', """./themes/azure-dark.tcl""")
+        style.theme_use('azure-dark')
+
+        # Softening Grid:
+        n_rows =1
         for i in range(n_rows):
             win.grid_rowconfigure(i,  weight =1)
-        for i in range(n_columns):
-            win.grid_columnconfigure(i,  weight =1)
+        win.grid_columnconfigure(1,  weight =1)
 
+        # Logo Canvas
+        self.LogoCanvas = Canvas(win, bg="blue")
+        self.LogoCanvas.pack(expand=1, fill=BOTH)
+        self.LogoCanvas.grid(row=0, column=0, columnspan=3)
+
+        paddingInt = 2
         # Directory
-        self.DirectoryInput = Entry(win)
-        self.DirectoryInput.grid(row=1, column=1, sticky=S+E+W, ipady=4)
+        self.DirectoryInput = ttk.Entry(win)
+        self.DirectoryInput.grid(row=1, column=1, sticky=S+E+W, ipady=5, pady=paddingInt, padx=paddingInt)
         self.DirectoryInput.insert(END, self.steamutils.gameDirectory)
-
-        self.DirectoryInputButton = Button(win, text=f'...', height = 1)
-        self.DirectoryInputButton.grid(row=1, column=2, sticky=S+W)
-
+        # Selector
+        self.DirectoryInputButton = ttk.Button(win, text=f'Select')
+        self.DirectoryInputButton.grid(row=1, column=0, sticky=S+E+W, ipady=4, pady=paddingInt, padx=paddingInt)
+        # DropDown
+        self.VersionDropDown = ttk.Combobox(win, textvariable=self.selectedVersion, width=5)
+        self.VersionDropDown.grid(row=1, column=2, sticky=S+E+W, pady=paddingInt, padx=paddingInt, ipady=5)
+        self.VersionDropDown['values'] = self.bepinutils.downloadVersions
+        self.VersionDropDown.set(self.bepinutils.downloadVersions[0])
+        
         # Install Button
-        self.installButton = Button(win, text=f'Install')
-        self.installButton.grid(row=2, column=1, sticky=E+W)
+        self.installButton = ttk.Button(win, text=f'Install')
+        self.installButton.grid(row=2, column=0, sticky=E+W, columnspan = 3, pady=paddingInt, padx=paddingInt)
+        self.installButton.bind('<Button-1>', self.install)
+
+    def install(self, args):
+        print(self.bepinutils.downloadURLs[self.bepinutils.downloadVersions.index(self.selectedVersion.get())])
+        
+        
 
         
-    def add(self):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1+num2
-        self.t3.insert(END, str(result))
-    def sub(self, event):
-        self.t3.delete(0, 'end')
-        num1=int(self.t1.get())
-        num2=int(self.t2.get())
-        result=num1-num2
-        self.t3.insert(END, str(result))
 
 window=Tk()
 mywin=GUIWindow(window)
