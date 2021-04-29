60bdf03e31aeb35b6ddb12ea0831a8b3b2ef9a2d
Amy Y <ctrlalternativedelete@gmail.com>
2021-04-29 22:25:48 +1000

    updated build script

--- buildscripts/PyInstallerBuild.sh
+++ buildscripts/PyInstallerBuild.sh
@@ -1,2 +1,6 @@
 pyinstaller ../srxdbepinexinstaller.py --onefile
-pyinstaller ../srxdbepinexinstallerui.pyw --add-data='../themes;./themes' --onefile
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    pyinstaller ../srxdbepinexinstallerui.pyw --add-data="../themes;./themes" --onefile
+elif [[ "$OSTYPE" == "msys" ]];
+    pyinstaller ../srxdbepinexinstallerui.pyw --add-data="../themes:./themes" --onefile
+fi
