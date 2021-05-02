6e2e607a9e07e0d8291b975bdff1ebfc382b3f42
Amy Y <ctrlalternativedelete@gmail.com>
2021-05-02 16:45:32 +1000

    added gui error handling

--- modules/gui.py
+++ modules/gui.py
@@ -1,6 +1,4 @@
 from tkinter import *
-import sys
-
 
 class GuiUtils:
     def __init__(self):
@@ -24,15 +22,24 @@ class GuiUtils:
 
 class PrintLogger(): # create file like object
     def __init__(self, textbox : Text): # pass reference to text widget
-        self.textbox = textbox # keep ref
-        self.textbox
+        try:
+            self.textbox = textbox # keep ref
+            self.textbox
+        except:
+            self.errormessage()
 
     def write(self, text):
-        self.textbox.config(state=NORMAL)
-        self.textbox.insert(END, text) # write text to textbox
-        self.textbox.config(state=DISABLED)
-        self.textbox.see(END)
+        try:
+            self.textbox.config(state=NORMAL)
+            self.textbox.insert(END, text) # write text to textbox
+            self.textbox.config(state=DISABLED)
+            self.textbox.see(END)
+        except:
+            self.errormessage()
             # could also scroll to end of textbox here to make sure always visible
 
     def flush(self): # needed for file like object
         pass
+
+    def errormessage(self):
+        raise Exception("Console could not be attatched.")
