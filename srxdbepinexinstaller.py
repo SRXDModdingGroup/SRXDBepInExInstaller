e904770f0996542f1a035a94093c839efa0b7bc4
Amy Y <ctrlalternativedelete@gmail.com>
2021-05-02 17:50:41 +1000

    enabled exception printing on cli

--- srxdbepinexinstaller.py
+++ srxdbepinexinstaller.py
@@ -5,40 +5,47 @@ from modules.bepinex import BepInExUtils
 from modules.installer import Installer
 from modules.args import ArgsUtils
 
-# Init Arg Vars
-uninstall = False
-# Parse Args
-argsutils = ArgsUtils()
-for index, arg in enumerate(argsutils.getArgs()):
-    if arg == "uninstall":
-        uninstall = True
+def main():
+    # Init Arg Vars
+    uninstall = False
+    # Parse Args
+    argsutils = ArgsUtils()
+    for index, arg in enumerate(argsutils.getArgs()):
+        if arg == "uninstall":
+            uninstall = True
 
-# Init Steam Module
-print("Getting Steam Info...")
-steamutils = SteamUtils()
-if(steamutils.gameDirectory == ""):
-    steamutils.inputPathIfEmpty()
-print(f"Found Game Path: {steamutils.gameDirectory}")
-
-# Init Installer Module
-installer = Installer(steamutils.gameDirectory)
-
-if (uninstall):
+    # Init Steam Module
+    print("Getting Steam Info...")
+    steamutils = SteamUtils()
     if(steamutils.gameDirectory == ""):
         steamutils.inputPathIfEmpty()
-    installer.uninstall()
+    print(f"Found Game Path: {steamutils.gameDirectory}")
 
-# Do rest of work:
-else:
-    # Init BepInEx Webpage
-    print("\nGetting Info from BepInEx Bleeding Edge Website...")
-    bepinutils = BepInExUtils()
+    # Init Installer Module
+    installer = Installer(steamutils.gameDirectory)
 
-    # Get Download URL
-    url = bepinutils.downloadURLs[bepinutils.downloadVersions.index("353")]
-    print(f"Found BepInExURL: {url}")
+    if (uninstall):
+        if(steamutils.gameDirectory == ""):
+            steamutils.inputPathIfEmpty()
+        installer.uninstall()
 
-    # Run installer
-    installer.install(bepinUrl=url)
+    # Do rest of work:
+    else:
+        # Init BepInEx Webpage
+        print("\nGetting Info from BepInEx Bleeding Edge Website...")
+        bepinutils = BepInExUtils()
+
+        # Get Download URL
+        url = bepinutils.downloadURLs[bepinutils.downloadVersions.index("353")]
+        print(f"Found BepInExURL: {url}")
+
+        # Run installer
+        installer.install(bepinUrl=url)
+try:
+    main()
+except Exception as e:
+    print(f'Exception: "{e}"')
+    input("Press any Key to Close...")
+
 
     
