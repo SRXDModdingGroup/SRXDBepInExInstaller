5a297a8aea3306f26d45ad0ca6b08475dba5bb94
Amy Y <ctrlalternativedelete@gmail.com>
2021-07-02 17:59:44 +1000

    changed default unity version

--- modules/unitylibs.py
+++ modules/unitylibs.py
@@ -2,7 +2,7 @@ from html.parser import HTMLParser
 
 class UnityLibsUtils:
     def __init__(self):
-        self.unityVersion = "2020.1.5"
+        self.unityVersion = "2020.3.8"
         self.githubRawUrl = f"https://github.com/LavaGang/Unity-Runtime-Libraries/raw/master/{self.unityVersion}.zip"
 
 
